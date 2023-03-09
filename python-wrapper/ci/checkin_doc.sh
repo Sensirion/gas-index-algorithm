11178cea55b7f38db88122c1404ca3d9e4ff04d1
lfierz <leonie.fierz@sensirion.com>
2023-03-09 14:50:10 +0100

    update gh pages deploy
    make sure to copy .nojekyll to gh pages branch
    do not fail check-in script if there are no changes to commit
    do not regenerate docs to deploy but reuse artifacts from build job
    delete .nojekyll, as it is created by sphinx ghpages extension

--- python-wrapper/ci/checkin_doc.sh
+++ python-wrapper/ci/checkin_doc.sh
@@ -4,7 +4,7 @@
 set -euv -o pipefail
 
 # clone the repo into a subfolder html, checkout the gh-pages into this folder and commit the freshly generated html
-git clone "git@gitlab:${CI_PROJECT_PATH}.git" html
+git clone "git@gitlab:${CI_PROJECT_PATH}.git"  html
 cd html
 git checkout gh-pages
 rm -f empty.txt
@@ -12,8 +12,15 @@ rm -f *.html
 rm -f *.js
 rm -rf _*
 cd ..
-mv docs/_build/html/* html/
+# make sure to copy .nojekyll
+cp -rf public/.[!.]*  public/* html
 cd html
 git add .
+
+if git diff-index --quiet HEAD --
+then
+  exit 0
+fi
+
 git commit -m"Automatic doc update: ${CI_COMMIT_SHORT_SHA}"
 git push
